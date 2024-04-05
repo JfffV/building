8226b7771942a27b5722bafcdf2c56e0363ec826
JfffV <ultionem14@gmail.com>
2024-04-05 16:02:53 +0200

    deleted footer in aboutUs.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,14 @@
 import React from "react";
-import './Footer.css';
+import "./Footer.css";
 
 export default function Footer() {
-  return( 
-  <footer> 
-    <div className="footer_container">
-      <span className="copyright">Copyright © 2024. All rights reserved &copy;</span>
-    </div>
+  return (
+    <footer>
+      <div className="footer_container">
+        <span className="copyright">
+          Copyright © 2024. All rights reserved &copy;
+        </span>
+      </div>
     </footer>
   );
 }
